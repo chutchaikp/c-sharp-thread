15bfa012acec2c6b9248a3dbbf17ebce60832f2d
chutchaikp <chutchai.kp@gmail.com>
2021-04-20 17:18:58 +0700

    Update thread using

--- Program.cs
+++ Program.cs
@@ -3,16 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace c_sharp_thread
 {
-    class Program
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    class ProgramBak
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi there!");
+            // 1. APM
+            //Console.ReadKey();
+            //AsyncromousDelegates();
+            //Console.ReadKey();
 
-            Console.ReadKey();
+            // 2. Thread Class
         }
+
+#region  1 APM - Asynchromous Programming Model
+        // Since at the beginning of .NET
+        
+        static async void BackgroundTask1(int p)
+        {  
+            for (int i = 0; i < p; i++)
+            {
+                Console.WriteLine($"task1 {i}");
+                // Task.Delay(3 * 1000).Wait();
+                await Task.Delay(3 * 1000); // for .Net 4.5
+            }
+        }
+        static void BackgroundTask2(int p)
+        {
+            for (int i = 0; i < p; i++)
+            {
+                Console.WriteLine($" --------- task2 {i}");
+                Task.Delay(500).Wait(); // 
+            }
+        }
+
+        static void OnCompleted(IAsyncResult result)
+        {
+            Console.WriteLine("Done!");
+            // Console.WriteLine(result);
+            
+        }
+
+        static void AsyncromousDelegates()
+        {
+            Action<int> task1 = BackgroundTask1;            
+            Action<int> task2 = BackgroundTask2;
+
+            task1.BeginInvoke(10, OnCompleted, null);
+            task2.BeginInvoke(13, OnCompleted, null);
+
+            // Anonymous method with Action delegate
+            //Action<int> task = delegate (int p) {
+            //    for (int i = 0; i < p; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //};
+            //task.BeginInvoke(13, OnCompleted, null);
+        }
+
+        #endregion
+
+        #region Thread Class
+        //  Since at the beginning of .NET
+        
+
+
+
+        #endregion
     }
 }
--- README.txt
+++ README.txt
@@ -1,7 +1,39 @@
 ﻿# C# Threading
+> Initiate tasks on another thread in .NET
 
-- 
-- 
-xx
-xx
-xx
+- Asyncromous Delegates - sincce the beginning of .NET
+```s
+Action<string> d = BackgroundTask;
+d.BeginInvoke("BeginInvoke", null, null);
+```
+
+- Thread Class 
+> Since the beginning of .NET
+> Use this approach if need a dedicated thread for a single task that is running for the lifetime 
+> of the application
+
+```s
+var t = new Thread(BackgroundTask);
+t.Name = "My Thread";
+t.Priority = ThreadPriority.AboveNormal;
+t.Start("Thread");
+```
+
+- ThreadPool - Since .NET 1.1
+> The method run on a thread from the thread pool just call `QueueUserWorkItem`
+> If there are free threads it will start immediately, otherwise it will be queued up
+> The disadvantage of this approach compared to the previous two is that it provides no mechanism for notification of when your task has finished. It’s up to you to report completion and catch exceptions.
+
+```s
+ThreadPool.QueueUserWorkItem(BackgroundTask, "ThreadPool");
+```
+
+- BackgroundWorker Component - Since .NET 2
+> It covers all the basics of reporting progress, cancellation, catching exceptions, and getting you back onto the UI thread so you can update the user interface
+
+```s
+backgroundWorker1.DoWork += BackgroundWorker1OnDoWork;
+```
+and within that function you are able to report progress:
+backgroundWorker1.ReportProgress(30, progressMessage);
+
